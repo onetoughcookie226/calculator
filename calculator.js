5543fe9aafaeeb4bf38ee97be3151985734565e6
onetoughcookie226 <onetoughcookie226@gmail.com>
2016-06-13 19:00:32 +0200

    second commit

---
+++ calculator.js
@@ -0,0 +1,9 @@
+// Get al the keys from the document
+
+// Add onclick event to all the keys and perform operation
+
+    // Get the input and button values
+
+// If clear event is pressed, erase everything
+
+// If eva key is pressed, calculate and display the result
