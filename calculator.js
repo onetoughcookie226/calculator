595342ae2962a7d3f51310ac973c40eaae43b461
onetoughcookie226 <onetoughcookie226@gmail.com>
2016-06-22 13:01:27 +0200

    add functionaity

--- calculator.js
+++ calculator.js
@@ -1,17 +1,33 @@
 
-    $(".screen").text("Results");
+   // $(".screen").text("Results");
 
-//Add a .click() listener to class clear,
-    $(".clear").click( function () {
+//Add a .click() listener to receive input from the buttons
+    //$(".clear").click( function () {
 
     // when it is clicked change the .text of .screen to "clear"
-        $(".screen").text("clear");
+      //  $(".screen").text("clear");
 
+    //});
+
+    var number = "";
+    var nwnumber = "";
+    var operator = "";
+   var total = "";
+
+    $(".keys").click(function(){
+        number += $(this).html;
+
+
+        $(".screen").text(number);
+        //  $(".keys").val();
+        //$(".screen").text("number");
     });
+   document.write("number");
+//take  .text and append i to the nmber
 
 
 
-// Get al the keys from the document
+    // Get al the keys from the document
 
 // Add onclick event to all the keys and perform operation
 
